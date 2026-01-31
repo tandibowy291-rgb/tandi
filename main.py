18a3fd2ea9a8c6947fc8899e182409bd2d8180b2
tandibowy291-rgb <tandibowy291@gmail.com>
2026-02-01 00:02:06 +0300

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,11 @@
+def on_on_update():
+    pass
+game.on_update(on_on_update)
+
+def on_on_update2():
+    pass
+game.on_update(on_on_update2)
+
+def on_update_interval():
+    pass
+game.on_update_interval(500, on_update_interval)
---
+++ main.ts
@@ -0,0 +1,9 @@
+game.onUpdate(function on_on_update() {
+    
+})
+game.onUpdate(function on_on_update2() {
+    
+})
+game.onUpdateInterval(500, function on_update_interval() {
+    
+})
